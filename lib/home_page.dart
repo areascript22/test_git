c78c9f4ade1fdb82047200483bbc4328cb868ce8
areascript <areascript22@gmail.com>
2025-08-08 05:45:49 -0500

    refactor: section was also added added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,8 +10,12 @@ class MyWidget extends StatefulWidget {
 class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
-    return  _buildSection1();
+    return  Column(children:[
+      _buildSection1(),
+      _buildecion2(),
+    ]);
   }
 
   Widget _buildSection1() => Text("Section1");
+  Widget _buildecion2() => Text("Sectino2");
 }
